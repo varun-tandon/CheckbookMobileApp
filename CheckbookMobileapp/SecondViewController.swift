ff931118d882fd70c2cc5afbaa65dd6826c06d71
varun-tandon <vrntandon@gmail.com>
2019-02-20 23:01:05 -0800

    Initial Commit

---
+++ CheckbookMobileapp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CheckbookMobileapp
+//
+//  Created by Varun on 2/20/19.
+//  Copyright © 2019 Varun Tandon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
